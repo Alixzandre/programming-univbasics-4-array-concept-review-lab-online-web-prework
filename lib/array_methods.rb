710e714b6359b8ceecc8cb57452601aed6f653e4
Alexander Bispo <alexbispo173@gmail.com>
2019-09-01 04:03:55 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -22,7 +22,7 @@ def find_max_value(array)
       count +=1
       
   end
-  
+  return max
 end
 
 def find_min_value(array)
@@ -35,5 +35,6 @@ def find_min_value(array)
     else
       count +=1
   end
+  return min
 
 end
