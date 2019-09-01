904150e7cc3b452a4250b92f7575c81b31730bb2
Alexander Bispo <alexbispo173@gmail.com>
2019-09-01 04:22:22 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,7 +2,7 @@ def find_element_index(array, value_to_find)
   count = 0
   index = nil
   while count < array.length do
-    if array[count] = value_to_find
+    if array[count] == value_to_find
       index = count
       return index
     else
